4b8ea8e88c1918df43424b7a706f205cb5165b7b
Search Power <myxbaah@searchpowerpvt.com>
2012-09-09 21:48:31 +0500

    About class modified

--- test2/classes/AboutWebPage.inc.php
+++ test2/classes/AboutWebPage.inc.php
@@ -4,15 +4,15 @@ class AboutUsWebPage extends HomeWebPage{
 	private $subLinks=array('Our company profile'=>'company.php','What customers are saying'=>'customers.php');
 	// implement 'buildLinks()' method
 	public function buildLinks() {
-		$output='<div id="navbar">'."n".'<h2>This is the navigation bar of the web page</h2>'."n".'<ul>'."n";
+		$output='<div id="navbar">'."\n".'<h2>This is the navigation bar of the web page</h2>'."\n".'<ul>'."\n";
 		foreach($this->mainLinks as $label=>$link) {
-			$output.='<li><a href="'.$link.'">'.$label.'</a></li>'."n";
+			$output.='<li><a href="'.$link.'">'.$label.'</a></li>'."\n";
 		}
 		$output.='</ul><ul>';
 		foreach($this->subLinks as $label=>$link) {
-			$output.='<li><a href="'.$link.'">'.$label.'</a></li>'."n";
+			$output.='<li><a href="'.$link.'">'.$label.'</a></li>'."\n";
 		}
-		$output.='</ul>'."n".'</div>';
+		$output.='</ul>'."\n".'</div>';
 		return $output;
    }
 }
